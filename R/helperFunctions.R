f01cc4df19053f78b8f531b937cb884c9e84d6c7
pieterjanvc <pieterjanvc@hotmail.com>
2025-01-09 19:45:43 -0500

    Extract mesh trees for authors and find overlap - start

--- R/helperFunctions.R
+++ R/helperFunctions.R
@@ -84,3 +84,50 @@ missingTreeNums = function(treenums){
 timeStamp = function(){
   format(Sys.time(), format = "%Y-%m-%d %H:%M:%S")
 }
+
+#' Extract the MeSH tree for a specific author
+#'
+#' @param conn Connection to the ColabNet database 
+#' @param auID Author ID (stored in the author table of the DB)
+
+#' @import dplyr
+#' @import stringr
+#'
+#' @return data frame with all MeSH treenums (including intermediate nodes)
+#' @export
+#'
+authorMeshTree <- function(conn, auID){
+  # Get all MeSH tree entries for the author
+
+  auMeshui <- tbl(conn, "author_affiliation") |> filter(auID == local(auID)) |> 
+    select(arID, auID) |> distinct() |> left_join(
+      tbl(conn, "mesh_article"), by = "arID" 
+    ) |> left_join(
+      tbl(conn, "meshLink"), by = "meshui"
+    ) |> left_join(
+      tbl(conn, "meshTree"), by = "uid"
+    ) |> 
+    # Ignore papers with no MeSH terms
+    filter(!is.na(meshui)) |> 
+    collect()
+
+  # Use the meshTree to get all intermediate meshuis
+  auTree <- auMeshui |> select(mtrID, uid, treenum) |> distinct() 
+
+  # Cut off one level of the treenum and find its parents
+  nextNums <- ifelse(str_detect(auTree$treenum, "\\."), 
+    str_remove(auTree$treenum, "\\.\\d+$"), NA) |> unique(na.rm = T)
+
+  while(length(nextNums) > 0){
+    new <- tbl(conn, "meshTree") |> filter(treenum %in% local(nextNums)) |> 
+      collect() 
+    newTreenums <- setdiff(new$treenum, auTree$treenum)
+    auTree <- bind_rows(auTree, new |> filter(treenum %in% newTreenums)) |> distinct()
+    nextNums <- ifelse(str_detect(newTreenums, "\\."), 
+      str_remove(newTreenums, "\\.\\d+$"), NA) |> unique(na.rm = T)
+  }
+
+  auTree <- auTree |> mutate(auID = as.integer({{auID}}), .before = 1)
+
+  return(auTree)
+}
