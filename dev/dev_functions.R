81d41d3b94150e680a14c21b134dd51f20f382ce
pieterjanvc <pieterjanvc@hotmail.com>
2025-08-14 18:49:17 -0400

    try and fix profvis dependency error

--- dev/dev_functions.R
+++ dev/dev_functions.R
@@ -1,15 +1,6 @@
 #' Wrapper function for profvis to work in Positron until issue resolved
 #' https://github.com/posit-dev/positron/issues/3269
 #'
-#' @param expr R expression to evaluate with profvis
-#' @param folder Default = "local". Folder where to store result files
-#'
-#' @import profvis
-#' @importFrom htmlwidgets saveWidget
-#' @importFrom rlang eval_tidy enquo
-#'
-#' @return Run profvis and open the results in the browser
-#'
 profvisRender <- function(expr, folder = "local") {
   folder = normalizePath(folder)
   temp_file <- tempfile(fileext = ".R")
